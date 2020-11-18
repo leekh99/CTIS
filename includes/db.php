f8a70205d5bfb53d4fb15330d4e6801a598b3a75
EdisonTan11 <59584716+EdisonTan11@users.noreply.github.com>
2020-11-18 22:03:17 +0800

    add manager

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
 $db['db_pass'] = "";
-$db['db_name'] = "";
+$db['db_name'] = "ctis";
 
 foreach($db as $key => $value){
     define(strtoupper($key),$value);
@@ -11,5 +11,22 @@ foreach($db as $key => $value){
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
+function addManager(){
+    $username = $_POST['manager_username'];
+    $name = $_POST['manager_name'];
+    $password = $_POST['manager_password'];
 
+    $queryUser = "INSERT INTO user(username, password, name)";
+    $queryUser .= " VALUES('{$username}', '{$password}', '{$name}')";
+
+    global $connection;
+    $connection->query($queryUser);
+
+    $queryCentreOfficer = "INSERT INTO centreofficer(username, position, workplace)";
+    $queryCentreOfficer .= " VALUES('{$username}','manager', 'null')";
+
+    $connection->query($queryCentreOfficer);
+    echo $connection->error;
+    $connection->close();
+}
 ?>
---
+++ views/js_superadmin/addManager.php
@@ -0,0 +1,38 @@
+<?php 
+
+if (isset($_POST['add_test'])){
+    addManager();
+    
+    
+}
+
+?>
+
+
+<script>
+    const loginName = document.getElementById('profile-name')
+
+    const checkUser = () =>{
+        localStorage.clear();
+        if (localStorage.length === 0){
+            let url = window.location.href;
+            url = url.slice(url.indexOf("user"), url.length);
+
+            let user = url.split('&');
+            let check = user[1].replace("check=", "");
+            user = user[0].replace("user=", "");
+
+            localStorage.setItem('loginUser', JSON.stringify({user,check}));
+
+            if (user)
+                elements.loginName.innerHTML = user;
+        }
+
+        const user = JSON.parse(localStorage.getItem('loginUser'));
+        if (user)
+            elements.loginName.innerHTML = user.user;
+    }
+        
+        
+    checkUser();
+</script>
